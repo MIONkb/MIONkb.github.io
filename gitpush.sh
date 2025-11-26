87c1a24c73348174a386f44aec7d994bf9c680f0
Mion <1063500258@qq.com>
2025-11-26 09:38:13 +0800

    adjust logo

--- gitpush.sh
+++ gitpush.sh
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 git checkout -b master
-=======
-git checkout master
->>>>>>> 17caecdcd10517396a56ede5f01efedaebe0ba7a
 git add -A
 git commit -m "add fudan sign to cv"
 git push -u origin master
